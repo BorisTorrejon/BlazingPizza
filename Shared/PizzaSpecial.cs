dd0229f364f232846682b418c17138d679df170a
Torrejon Boris <torrejonboris@gmail.com>
2021-08-27 21:58:14 -0300

    creacion loadingbar-slide
    blaqueo de proyecto Webassembly y creacion de loadingbar-slide

---
+++ Shared/PizzaSpecial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazingPizza.Shared
+{
+    public class PizzaSpecial
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal BasePrice { get; set; }
+        public string Description { get; set; }
+    }
+}
